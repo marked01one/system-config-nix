de1578360eb523e4a63332029187d84927f5c731
marked01one <minhkhoitran2k3@gmail.com>
2026-04-01 02:19:39 -0700

    scripts: added eza-grep.

--- modules/features/scripts.nix
+++ modules/features/scripts.nix
@@ -5,14 +5,27 @@ in {
   in {
     environment.systemPackages = with local-pkgs; [
       fcp
+      eza-grep
     ];
   };
 
-  flake.homeModules.scripts = {pkgs, ...}: let
+  flake.homeModules.scripts = {
+    pkgs,
+    lib,
+    config,
+    ...
+  }: let
     local-pkgs = self.packages.${pkgs.stdenv.hostPlatform.system};
   in {
-    home.packages = with local-pkgs; [
-      fcp
-    ];
+    home.packages = with local-pkgs;
+      [
+        fcp
+        eza-grep
+      ]
+      ++ (
+        # Only include the following packages if Stylix is enabled AND defined.
+        lib.optionals (config ? stylix && config.stylix.enable)
+        (with local-pkgs; [print-stylix])
+      );
   };
 }

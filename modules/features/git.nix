591df5229986f9bea1b6e7b069ffcb0d2861b540
marked01one <minhkhoitran2k3@gmail.com>
2026-04-02 19:59:52 -0700

    delta: syntax highlighter for git diffs.

--- modules/features/git.nix
+++ modules/features/git.nix
@@ -1,14 +1,27 @@
-{...}: {
+{self, ...}: {
   flake.nixosModules.git = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
       git
       git-lfs
+    ];
+
+    home-manager.users.marked01one.imports = [
+      self.homeModules.git
     ];
   };
 
   flake.homeModules.git = {...}: {
     programs.git.enable = true;
     programs.git.lfs.enable = true;
+
+    # Syntax highlighter for git diffs.
+    programs.delta = {
+      enable = true;
+      enableGitIntegration = true;
+      options = {};
+    };
+
+    # Git config in ~/.config/git/config
     programs.git.settings = {
       user.name = "marked01one";
       user.email = "mnihkhoitran2k3@gmail.com";

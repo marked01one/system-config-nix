a3ccc49e2c29d82772c74005140ba99f0f03bc85
marked01one <minhkhoitran2k3@gmail.com>
2026-04-02 20:00:30 -0700

    eza-grep: refactor to move usage text outside of script.

--- modules/packages/scripts/eza-grep.nix
+++ modules/packages/scripts/eza-grep.nix
@@ -1,28 +1,33 @@
 {...}: {
   perSystem = {pkgs, ...}: {
-    packages.eza-grep = pkgs.writeShellApplication {
-      name = "eza-grep";
-      runtimeInputs = with pkgs; [eza];
-      text = ''
-        # syntax: shell
-        function usage {
-          echo ""
-          echo "Usage: eza-grep [DIRECTORY] [SEARCH]"
-          echo ""
-          echo "List out a directory, filtered by the search query"
-          exit 1
-        }
+    packages.eza-grep = let
+      usageText = ''
 
-        if [[ "$#" -lt 2 ]]; then
-          echo "Not enough arguments! Must be exactly two (2)"
-          usage
-        elif [[ "$#" -gt 2 ]]; then
-          echo "Too many arguments! Must be exactly two (2)"
-          usage
-        fi
+        Usage: eza-grep [DIRECTORY] [SEARCH]
 
-        eza --icons=always --color=always "$1" | grep "$2"
+        List a directory, filtered by the search query
       '';
-    };
+    in
+      pkgs.writeShellApplication {
+        name = "eza-grep";
+        runtimeInputs = with pkgs; [eza];
+        text = ''
+          # syntax: shell
+          function usage {
+            echo "${usageText}"
+            exit 1
+          }
+
+          if [[ "$#" -lt 2 ]]; then
+            echo "Not enough arguments! Must be exactly two (2)"
+            usage
+          elif [[ "$#" -gt 2 ]]; then
+            echo "Too many arguments! Must be exactly two (2)"
+            usage
+          fi
+
+          eza --icons=always --color=always "$1" | grep "$2"
+        '';
+      };
   };
 }

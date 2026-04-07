4ae5e10b086c182f33739f07d158d3c89000a6e3
marked01one <minhkhoitran2k3@gmail.com>
2026-04-07 14:06:51 -0700

    dotnet-sdk: updated dotnet to dotnet 10.

--- modules/features/dotnet-sdk.nix
+++ modules/features/dotnet-sdk.nix
@@ -1,9 +1,9 @@
 {...}: {
   flake.nixosModules.dotnet-sdk = {pkgs, ...}: {
-    environment.systemPackages = with pkgs; [dotnet-sdk];
+    environment.systemPackages = with pkgs; [dotnet-sdk_10];
   };
 
   flake.homeModules.dotnet-sdk = {pkgs, ...}: {
-    home.packages = with pkgs; [dotnet-sdk];
+    home.packages = with pkgs; [dotnet-sdk_10];
   };
 }

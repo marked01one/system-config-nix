da43cfff0175d0d73514796a4acaa82e7ad323e1
marked0one <minhkhoitran2k3@gmail.com>
2026-04-20 14:21:53 -0700

    hydrogen: initialize flake-parts config.

--- modules/clients/hydrogen.nix
+++ modules/clients/hydrogen.nix
@@ -12,13 +12,121 @@
     ];
   };
 
-  flake.nixosModules.hydrogen = {...}: {
-    imports = with self.nixosModules; [
-      home-manager
+  flake.nixosModules.hydrogen = {
+    config,
+    pkgs,
+    ...
+  }: {
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    # Use latest kernel.
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    networking.hostName = "hydrogen"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "America/Los_Angeles";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+
+    # Configure keymap in X11
+    services.xserver.xkb.layout = "us";
+      services.xserver.xkb.variant = "";
+      
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.marked01one = {
+      isNormalUser = true;
+      description = "marked01one";
+      extraGroups = ["networkmanager" "wheel"];
+      packages = with pkgs; [];
+    };
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    environment.systemPackages = with pkgs; [
+      neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      wget
+      git
     ];
 
-    services.printing.enable = true;
+    # Enable tailscale on this machine
+    services.tailscale.enable = true;
+
+    # Enable the OpenSSH daemon.
+    services.openssh.enable = true;
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    system.stateVersion = "25.11";
+
+    nix = {
+      settings = { 
+  	allowed-users = ["marked01one"];
+	experimental-features = ["nix-command" "flakes"];
+      };
+      nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    };
   };
 
-  flake.nixosModules.hydrogen-hardware = {};
+  flake.nixosModules.hydrogen-hardware = {
+    config,
+    lib,
+    pkgs,
+    modulesPath,
+    ...
+  }: {
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+    boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-amd"];
+    boot.extraModulePackages = [];
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/bb40fd15-8184-4c52-b92f-1df4c2360c36";
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/DFAB-FCC2";
+      fsType = "vfat";
+      options = ["fmask=0077" "dmask=0077"];
+    };
+
+    swapDevices = [];
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }

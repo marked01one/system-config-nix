2706362173694901536e0de1768ee0685e9235d5
marked01one <minhkhoitran2k3@gmail.com>
2026-04-25 17:17:03 -0700

    marke01one.admin: added an admin account.

---
+++ modules/users/marked01one.admin.nix
@@ -0,0 +1,28 @@
+{self, ...}: {
+  flake.nixosModules."marked01one.admin" = {...}: {
+    users.users."marked01one.admin" = {
+      enable = true;
+      isNormalUser = true;
+      description = "marke01one.admin";
+      extraGroups = ["networkmanager" "wheel"];
+    };
+
+    # Home Manager module imports.
+    home-manager.users."marked01one.admin".imports = with self.homeModules; [
+      # Core user home module.
+      marked01one.admin
+    ];
+  };
+
+  flake.homeModules."marked01one.admin" = {...}: let
+    username = "marked01one.admin";
+  in {
+    # Username for the user of this home manager.
+    home.username = username;
+
+    # The path to the home directory.
+    home.homeDirectory = "/home/${username}";
+    home.stateVersion = "26.05";
+    home.enableNixpkgsReleaseCheck = false;
+  };
+}

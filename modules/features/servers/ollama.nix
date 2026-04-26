d071aea3c24da14262695e5884d491903020a95f
marked01one <minhkhoitran2k3@gmail.com>
2026-04-25 20:44:00 -0700

    ollama: open firewall to vpn.

--- modules/features/servers/ollama.nix
+++ modules/features/servers/ollama.nix
@@ -6,10 +6,13 @@
         "gemma3:270m"
         "gemma3:1b"
         "qwen2.5:0.5b"
+        "gpt-oss:20b"
       ];
       # Remove any models not declared in `loadModels`
       syncModels = true;
       openFirewall = true;
+      host = "0.0.0.0";
+      port = 11434;
     };
   };
 }

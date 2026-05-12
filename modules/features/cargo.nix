88a4c549b7fb43122471c9dbbcd6e35d438f3843
marked01one <minhkhoitran2k3@gmail.com>
2026-05-11 21:29:09 -0700

    cargo: moved to rustup toolchain.

--- modules/features/cargo.nix
+++ modules/features/cargo.nix
@@ -1,9 +1,15 @@
 {...}: {
   flake.nixosModules.cargo = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
-      cargo
       gcc
+      rust-analyzer
+      rustup
     ];
+
+    environment.sessionVariables = {
+      LIBTORCH_LIB = "${pkgs.libtorch-bin}";
+      LIBTORCH_INCLUDE = "${pkgs.libtorch-bin.dev}";
+    };
   };
 
   flake.homeModules.cargo = {...}: {
